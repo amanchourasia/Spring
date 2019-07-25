7a103460c9e47719a4e033255fc8331b59ebaed7
Hemant Tiwari <50255261+amanhemant01@users.noreply.github.com>
2019-07-25 19:56:40 +0530

    Update Application.java

--- src/main/java/manisa/application/Application.java
+++ src/main/java/manisa/application/Application.java
@@ -17,8 +17,8 @@ public class Application {
     @Bean
     CommandLineRunner init(UserRepository userRepository) {
         return args -> {
-            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
-                User user = new User(name, name.toLowerCase() + "@domain.com");
+            Stream.of("Hemant", "Ankush", "Manisa", "Ankana", "Bizeet").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@wipro.com");
                 userRepository.save(user);
             });
             userRepository.findAll().forEach(System.out::println);
